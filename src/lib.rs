4c139ee517b22593101455e91f7ef99bacb5afc8
Dayvson Spacca <spacca.dayvson@gmail.com>
2024-08-30 12:20:42 -0300

    feat: add OrganizeMethod for Extensions

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
-use std::collections::HashMap;
+use std::{
+    collections::HashMap,
+    ffi::OsStr,
+    fs::{copy, create_dir_all, read_dir},
+    path::Path,
+};
 
 #[derive(Debug)]
 pub enum OrganizeMethod {
@@ -18,6 +23,68 @@ pub enum OrganizeMethod {
 impl OrganizeMethod {
     pub fn custom_extension(extensions: HashMap<&'static str, Vec<&'static str>>) -> Self {
         OrganizeMethod::Extension(extensions)
+    }
+
+    fn execute(self, _parallel: bool, path: &'static str) {
+        let files = self.get_files(path);
+
+        if let OrganizeMethod::Extension(extensions) = self {
+            for file_name in files {
+                if let Some(extension) = file_name.split('.').last() {
+                    for (folder_name, ext_list) in &extensions {
+                        if ext_list.contains(&extension) {
+                            let folder_path = Path::new(path).join(folder_name);
+
+                            if let Err(e) = create_dir_all(&folder_path) {
+                                eprintln!(
+                                    "Erro ao criar diretório {}: {}",
+                                    folder_path.display(),
+                                    e
+                                );
+                            }
+
+                            let file_path = Path::new(path).join(&file_name);
+                            if !file_path.exists() {
+                                eprintln!("Arquivo não encontrado: {}", file_path.display());
+                                continue;
+                            }
+
+                            let dest_path =
+                                folder_path.join(file_path.file_name().unwrap_or(OsStr::new("")));
+
+                            if let Err(e) = copy(file_path, &dest_path) {
+                                eprintln!(
+                                    "Erro ao copiar arquivo {} para {}: {}",
+                                    file_name,
+                                    dest_path.display(),
+                                    e
+                                );
+                            }
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    fn get_files(&self, path: &'static str) -> Vec<String> {
+        if let OrganizeMethod::Extension(_) = self {
+            let mut file_names = Vec::new();
+            if let Ok(entries) = read_dir(path) {
+                for entry in entries.flatten() {
+                    let file_path = entry.path();
+                    if file_path.is_file() {
+                        if let Some(file_name) = file_path.file_name().and_then(|f| f.to_str()) {
+                            file_names.push(file_name.to_string());
+                        }
+                    }
+                }
+            }
+            file_names
+        } else {
+            Vec::new()
+        }
     }
 }
 
@@ -49,11 +116,11 @@ impl OrganizeOptions {
     pub fn default() -> Self {
         let mut default_extensions: HashMap<&'static str, Vec<&'static str>> = HashMap::new();
 
-        default_extensions.insert("Documents", vec![".pdf", ".txt", ".docs"]);
-        default_extensions.insert("Images", vec![".png", ".jpg", ".jpeg"]);
-        default_extensions.insert("Audios", vec![".mp3", ".wav", ".flac"]);
-        default_extensions.insert("Videos", vec![".mp4", ".mov", ".avi"]);
-        default_extensions.insert("Sheets", vec![".csv", ".xlsx", ".ods"]);
+        default_extensions.insert("Documents", vec!["pdf", "txt", "docs"]);
+        default_extensions.insert("Images", vec!["png", "jpg", "jpeg"]);
+        default_extensions.insert("Audios", vec!["mp3", "wav", "flac"]);
+        default_extensions.insert("Videos", vec!["mp4", "mov", "avi"]);
+        default_extensions.insert("Sheets", vec!["csv", "xlsx", "ods"]);
 
         Self {
             method: OrganizeMethod::Extension(default_extensions),
@@ -69,5 +136,39 @@ impl OrganizeOptions {
     /// Especifica se o método de organização deve rodar em paralelo quando possível ou não.
     pub fn set_parallel(&mut self, parallel: bool) {
         self.parallel = parallel;
+    }
+}
+
+pub struct Organize {
+    options: OrganizeOptions,
+    path: &'static str,
+}
+
+impl Organize {
+    pub fn new(path: &'static str, options: OrganizeOptions) -> Self {
+        Self { path, options }
+    }
+
+    pub fn set_path(&mut self, path: &'static str) {
+        self.path = path;
+    }
+
+    pub fn set_options(&mut self, options: OrganizeOptions) {
+        self.options = options;
+    }
+
+    pub fn execute(self) -> Result<(), std::io::Error> {
+        if self.path.is_empty() {
+            return Err(std::io::Error::new(
+                std::io::ErrorKind::InvalidInput,
+                "Path cannot be empty",
+            ));
+        }
+
+        self.options
+            .method
+            .execute(self.options.parallel, self.path);
+
+        Ok(())
     }
 }
